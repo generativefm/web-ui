686c6507dc25c17565109b21cfef3b1b7f85dc29
metalex9 <alexbainter@gmail.com>
2021-05-30 18:16:56 -0500

    Make top bar shadow optional

--- src/top-bar/full-top-bar.jsx
+++ src/top-bar/full-top-bar.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import classnames from 'classnames';
 import styles from './full-top-bar.module.scss';
 
-const FullTopBar = ({ productName, navLinks = [], children }) => {
+const FullTopBar = ({
+  productName,
+  navLinks = [],
+  children,
+  hasShadow = true,
+}) => {
   return (
-    <header className={styles['full-top-bar']}>
+    <header
+      className={classnames(styles['full-top-bar'], {
+        [styles['full-top-bar--has-shadow']]: hasShadow,
+      })}
+    >
       <div className={styles.title}>
         Generative.fm{' '}
         <span className={styles['title__product-name']}>{productName}</span>
@@ -36,6 +46,7 @@ FullTopBar.propTypes = {
     })
   ),
   children: PropTypes.node,
+  hasShadow: PropTypes.bool,
 };
 
 export default FullTopBar;
--- src/top-bar/mini-top-bar.jsx
+++ src/top-bar/mini-top-bar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import styles from './mini-top-bar.module.scss';
 
-const MiniTopBar = ({ productName, children }) => {
+const MiniTopBar = ({ productName, children, hasShadow = true }) => {
   return (
-    <header className={styles['mini-top-bar']}>
+    <header
+      className={classnames(styles['mini-top-bar'], {
+        [styles['mini-top-bar--has-shadow']]: hasShadow,
+      })}
+    >
       <div className={styles['mini-top-bar__title']}>
         Generative.fm{' '}
         <span className={styles['mini-top-bar__title__product-name']}>
@@ -19,6 +24,7 @@ const MiniTopBar = ({ productName, children }) => {
 MiniTopBar.propTypes = {
   productName: PropTypes.string.isRequired,
   children: PropTypes.node,
+  hasShadow: PropTypes.bool,
 };
 
 export default MiniTopBar;
